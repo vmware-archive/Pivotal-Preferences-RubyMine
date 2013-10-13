db66e3b0985f28e570da868b464cc3bfe2ffbc7b
Matt Parker <moonmaster9000@gmail.com>
2013-10-13 07:24:35 -0700

    remove target files before creating the symlinks

---
+++ lib/mine_prefs/target_file_remover.rb
@@ -0,0 +1,19 @@
+module MinePrefs
+  class TargetFileRemover
+    attr_reader :filesystem
+
+    def initialize(filesystem: FileUtils)
+      @filesystem = filesystem
+    end
+
+    def prepare_installation(installation_bundle)
+      installation_bundle.files_to_install.each do |file_to_install|
+        begin
+          filesystem.rm_rf(File.join(installation_bundle.target, file_to_install))
+        rescue Errno::ENOENT
+        end
+      end
+    end
+  end
+end
+
