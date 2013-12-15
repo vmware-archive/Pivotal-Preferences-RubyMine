7192a503156b2d1f88a37c9e758df26920d0addd
Matt Parker <moonmaster9000@gmail.com>
2013-12-14 20:18:02 -0800

    Cleaning up aliased method names

--- lib/mine_prefs/method_hook.rb
+++ lib/mine_prefs/method_hook.rb
@@ -2,14 +2,14 @@ module MinePrefs
   class MethodHook
     def initialize(klass: nil, method_name: nil, before: ->(*) {}, after: ->(*) {})
       klass.class_eval do
-        define_method "#{method_name}_with_logging" do |*args|
+        define_method "#{method_name}_with_method_hook" do |*args|
           before.call *args
-          result = send "#{method_name}_without_logging", *args
+          result = send "#{method_name}_without_method_hook", *args
           after.call *args, result
         end
 
-        alias_method "#{method_name}_without_logging", method_name
-        alias_method method_name, "#{method_name}_with_logging"
+        alias_method "#{method_name}_without_method_hook", method_name
+        alias_method method_name, "#{method_name}_with_method_hook"
       end
     end
   end
